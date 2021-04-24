8cf16f2686805104d5f29aa81b64fee7e964d501
DaniyarKulmanov <DanRubyDeveloper@gmail.com>
2021-04-24 13:20:08 +0600

    merged bb43e89 commit from branch other

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,6 +1,7 @@
-class ArticlesController < ApplicationController
+# frozen_string_literal: true
 
-  http_basic_authenticate_with name: "dhh", password: "secret", except: [:index, :show]
+class ArticlesController < ApplicationController
+  http_basic_authenticate_with name: 'dhh', password: 'secret', except: %i[index show]
 
   def index
     @articles = Article.all
